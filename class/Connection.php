1c3047f3901023615f721262014b3a481874188f
Ahmed Mohamed <ahmed7777003330@gmail.com>
2024-08-06 10:20:55 +0300

    first commit

---
+++ class/Connection.php
@@ -0,0 +1,8 @@
+<?php
+        define('db_name' , 'Book');
+        define('User_name' , 'root');
+        define('Pass' , '');
+        define('Host' , 'localhost');
+    class Connection{
+        
+    }
